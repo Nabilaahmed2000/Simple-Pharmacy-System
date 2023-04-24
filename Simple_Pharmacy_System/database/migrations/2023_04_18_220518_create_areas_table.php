b2f930f05f038176f492c8629c2986f09d3e35b4
Mariam Zayed <66179261+MariamZayed@users.noreply.github.com>
2023-04-24 17:49:34 +0200

    Update 2023_04_18_220518_create_areas_table.php

--- Simple_Pharmacy_System/database/migrations/2023_04_18_220518_create_areas_table.php
+++ Simple_Pharmacy_System/database/migrations/2023_04_18_220518_create_areas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('areas', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-
+            $table->foreignId('country_id')->constrained('countries');
             $table->timestamps();
         });
     }
