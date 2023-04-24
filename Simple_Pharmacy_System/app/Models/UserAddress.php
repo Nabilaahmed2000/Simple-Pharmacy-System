08ccce9a607796a54f9c4921e2b78f198b3b29c9
Mariam Zayed <66179261+MariamZayed@users.noreply.github.com>
2023-04-24 17:32:39 +0200

    Create UserAddress.php

---
+++ Simple_Pharmacy_System/app/Models/UserAddress.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'area_id',
+        'street_name',
+        'building_number',
+        'floor_number',
+        'flat_number',
+        'is_main',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class);
+    }
+}
